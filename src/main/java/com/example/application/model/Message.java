8fd8052ae12b8c6e80683a6bc7387c93ea4f6580
Syed Azmeer <syed.azmeer2111@gmail.com>
2023-02-21 15:17:48 +0800

    Define data model

--- src/main/java/com/example/application/model/Message.java
+++ src/main/java/com/example/application/model/Message.java
@@ -1,7 +1,40 @@
 package com.example.application.model;
 
+import reactor.util.annotation.NonNull;
+
+import java.time.Instant;
+
 public class Message {
 
+    @NonNull
+    private String text;
+    private Instant time;
+    @NonNull
+    private String useName;
 
-    private String[] test = new String[20];
+    @NonNull
+    public String getText() {
+        return text;
+    }
+
+    public void setText(@NonNull String text) {
+        this.text = text;
+    }
+
+    public Instant getTime() {
+        return time;
+    }
+
+    public void setTime(Instant time) {
+        this.time = time;
+    }
+
+    @NonNull
+    public String getUseName() {
+        return useName;
+    }
+
+    public void setUseName(@NonNull String useName) {
+        this.useName = useName;
+    }
 }
